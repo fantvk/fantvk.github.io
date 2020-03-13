04445eb8d5d579c692c833c6ce1847e88ccc0ce5
fantvk <fant.vk@gmail.com>
2020-03-14 00:19:07 +0200

    revolut-test-task

---
+++ precache-manifest.300b96c22d40d0ffef1930a7d83a08a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb2215d1b88b8deb453570785798409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe484d343ce2c964c21f",
+    "url": "/static/css/2.a1c502db.chunk.css"
+  },
+  {
+    "revision": "15d571e1aa3e40790e5f",
+    "url": "/static/css/main.05267858.chunk.css"
+  },
+  {
+    "revision": "fe484d343ce2c964c21f",
+    "url": "/static/js/2.21a9d3a4.chunk.js"
+  },
+  {
+    "revision": "7c732e01c0bd675c4463b1a6ac697ed7",
+    "url": "/static/js/2.21a9d3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d571e1aa3e40790e5f",
+    "url": "/static/js/main.183824bb.chunk.js"
+  },
+  {
+    "revision": "bf1d96ac85b5cd816c0c",
+    "url": "/static/js/runtime-main.a95e0a4a.js"
+  }
+]);
